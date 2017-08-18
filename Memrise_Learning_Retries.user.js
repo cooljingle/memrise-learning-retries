51c1df7ed3eb6f7e924cf729d506aa68d6ace660
Aaron Miles <cool_jingle@hotmail.co.uk>
2017-08-18 20:08:08 +0100

    fixed script breaking due to memrise update

--- Memrise_Learning_Retries.user.js
+++ Memrise_Learning_Retries.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://github.com/cooljingle
 // @description    Keep incorrectly answered words part of the learning session
 // @match          https://www.memrise.com/course/*/garden/learn*
-// @version        0.0.3
+// @version        0.0.4
 // @updateURL      https://github.com/cooljingle/memrise-learning-retries/raw/master/Memrise_Learning_Retries.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-learning-retries/raw/master/Memrise_Learning_Retries.user.js
 // @grant          none
@@ -20,9 +20,12 @@ $(document).ready(function() {
                     b._list = boxesList; //overwrite new box arrangement
                     var wrongBox = b.current();
                     if(currentBox.learn_session_level){
-                        var numPlacesAhead = Math.min(3, Math.max(1, wrongBox.thinguser.total_streak));
+                        var numPlacesAhead = Math.min(3, Math.max(1, wrongBox.thinguser &&  wrongBox.thinguser.total_streak));
                         b._list.splice(Math.min(b.num + 1 + numPlacesAhead, b._list.length - 1), 0, currentBox); //add retest
                         b.reorder_future_to_avoid_repeats((x, y) => b.same_thinguser(x, y) && b.same_thinguser(currentBox, x));
+                        MEMRISE.garden.boxes._list.sort(function(a, b){
+                            return (a.thing_id === b.thing_id && a.learn_session_level && b.learn_session_level && a.learn_session_level > b.learn_session_level) ? 1: 0;
+                        });
                     }
                     b.add_next({ template: wrongBox.template, thing_id: currentBox.thing_id, column_a: currentBox.column_a, column_b: currentBox.column_b, updates_scheduling: false, isFakeInsert: true }); //add word test
                     b.add_next({ template: "copytyping", thing_id: currentBox.thing_id, column_a: currentBox.column_a, column_b: currentBox.column_b, wrote: wrongBox.given_answer }); //add word reminder
@@ -37,8 +40,7 @@ $(document).ready(function() {
                     };
                 }());
 
-                _(MEMRISE.garden).on("wrong", setBoxes);
-                _(MEMRISE.garden).on("nearly", setBoxes);
+                MEMRISE.garden.session.onWrong = MEMRISE.garden.session.onNearly = setBoxes;
             }
 
             return cached_function.apply(this, arguments);
