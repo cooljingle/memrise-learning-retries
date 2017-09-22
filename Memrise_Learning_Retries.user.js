e92af873e456421d979edb7ba89e09e06b1753cf
Aaron Miles <cool_jingle@hotmail.co.uk>
2017-09-22 03:15:37 +0100

    more script fixes, avoidRepeats func

--- Memrise_Learning_Retries.user.js
+++ Memrise_Learning_Retries.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://github.com/cooljingle
 // @description    Keep incorrectly answered words part of the learning session
 // @match          https://www.memrise.com/course/*/garden/learn*
-// @version        0.0.7
+// @version        0.0.8
 // @updateURL      https://github.com/cooljingle/memrise-learning-retries/raw/master/Memrise_Learning_Retries.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-learning-retries/raw/master/Memrise_Learning_Retries.user.js
 // @grant          none
@@ -25,12 +25,12 @@ $(document).ready(function() {
                         var numPlacesAhead = Math.min(
                             3, //hard cap
                             Math.max(1, wrongBox.thinguser && wrongBox.thinguser.total_streak), //streak based cap
-                            _.findIndex(b._list, function(box, i){ return (i > b.num && box.learnable_id === wrongBox.learnable_id && box.learn_session_level) || i === b._list.length; }) - b.num //next reoccurence cap
+                            Math.max(1, _.findIndex(b._list, function(box, i){ return (i > b.num && box.learnable_id === wrongBox.learnable_id && box.learn_session_level) || i === b._list.length; }) - b.num) //next reoccurence cap
                         );
                         b._list.splice(Math.min(b.num + 1 + numPlacesAhead, b._list.length - 1), 0, currentBox); //add retest
                     }
-                    b.add_next({ template: wrongBox.template, learnable_id: currentBox.learnable_id, isFakeInsert: true }); //add extra fake word test
-                    s.addPresentationOnMistake(wrongBox); //add word reminder
+                    s.addBoxesAfterMistake(wrongBox); //add word reminder
+                    avoidRepeats();
                 };
                 b.activate_box = (function() {
                     var cached_function = b.activate_box;
@@ -42,20 +42,24 @@ $(document).ready(function() {
                     };
                 }());
 
-                g.register = (function() {
-                    var cached_function = g.register;
-                    return function() {
-                        if(arguments[1] === 1 && arguments[0].box_dict.isFakeInsert) //don't register fake inserts
-                            return;
-                        else
-                            return cached_function.apply(this, arguments);
-                    };
-                }());
-
                 s.onWrong = s.onNearly = setBoxes;
             }
 
             return cached_function.apply(this, arguments);
         };
     }());
+
+    function avoidRepeats() {
+        var shouldSwap;
+        for(let i = 1; i < b._list.length - 1; i++) {
+            var x = b._list[i-1],
+                y = b._list[i],
+                z = b._list[i+1],
+                yzSwap = x.learnable_id === y.learnable_id &&
+                y.learnable_id !== z.learnable_id &&
+                !_.some(["presentation", "copytyping"], t => t === y.template);
+            if(yzSwap)
+                [b._list[i], b._list[i+1]] = [b._list[i+1], b._list[i]];
+        }
+    }
 });
